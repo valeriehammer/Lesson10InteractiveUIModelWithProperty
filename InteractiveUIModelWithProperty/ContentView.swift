1828f1cca70489a3bdfa61715addc2a7ee43d300
Valerie Hammer <vkellogg@wisc.edu>
2025-06-03 20:36:52 -0500

    Initial Commit

---
+++ InteractiveUIModelWithProperty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InteractiveUIModelWithProperty
+//
+//  Created by Valerie Hammer on 6/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InteractiveUIModelWithProperty/InteractiveUIModelWithPropertyApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIModelWithPropertyApp.swift
+//  InteractiveUIModelWithProperty
+//
+//  Created by Valerie Hammer on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIModelWithPropertyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
